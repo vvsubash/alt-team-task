7d8f49027346f1603d19d1e508f237674b687826
vvsubash <subash.venkat007@gmail.com>
2024-01-07 19:42:31 +0530

    * feat(app.vue): add todo list rendering and fetching todos from baseStore

--- app.vue
+++ app.vue
@@ -2,4 +2,11 @@
   <div>
     <h1>Simple Todo List</h1>
   </div>
+  <ul>
+    <li v-for="todo in baseStore.todos" :key="todo.id">{{ todo.id }} - {{ todo.title }}</li>
+  </ul>
 </template>
+<script setup lang="ts">
+const baseStore = useBaseStore()
+await baseStore.getTodos()
+</script>
