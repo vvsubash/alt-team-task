f8894b73212f53ed5afc0aa7ff124fd3ea8eee0d
vvsubash <subash.venkat007@gmail.com>
2024-01-07 20:10:37 +0530

    * chore(app.vue): add language selection dropdown * feat(app.vue): add strike-through style for completed todos * style(app.vue): add styling for app.vue

--- app.vue
+++ app.vue
@@ -1,12 +1,45 @@
 <template>
+  <div class="flex rev-row">
+    <select name="locale" id="locale" v-model="locale">
+      <option value="en" selected>
+        English
+      </option>
+      <option value="te">
+        Telugu
+      </option>
+    </select>
+  </div>
   <div>
-    <h1>Simple Todo List</h1>
+    <h1>{{ $t('mainTitle') }}</h1>
   </div>
   <ul>
-    <li v-for="todo in baseStore.todos" :key="todo.id">{{ todo.id }} - {{ todo.title }}</li>
+    <li v-for="todo in baseStore.todos" :key="todo.id" :class="`${todo.completed ? 'strike': ''}`">{{ todo.id }} - {{ todo.title }}</li>
   </ul>
 </template>
 <script setup lang="ts">
 const baseStore = useBaseStore()
 await baseStore.getTodos()
+const { locale } = useI18n()
 </script>
+<style>
+body {
+  max-width: 50ch;
+  margin: auto
+}
+.flex {
+  display: flex;
+}
+.strike {
+  text-decoration: line-through;
+}
+
+.rev-row {
+  justify-content: end;
+}
+h1 {
+  margin-bottom: 2rem;
+}
+li {
+  list-style: none;
+}
+</style>
