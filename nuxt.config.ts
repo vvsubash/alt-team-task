2c6c24322c261c419d8d04660ea0a2a58385c2a4
vvsubash <subash.venkat007@gmail.com>
2024-01-07 19:43:12 +0530

    * feat(nuxt.config.ts): add pinia configuration for store directories * feat(nuxt.config.ts): add awsm.css stylesheet to app head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
   modules: [
     '@pinia/nuxt'
-  ]
+  ],
+  pinia: {
+    storesDirs: [
+      './store/**s'
+    ]
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://unpkg.com/awsm.css/dist/awsm.min.css",
+          type:"text/css"
+        }
+      ]
+    }
+  }
 })
